89b5572b330e51e1697a4523a685ab4b4e3d2bf8
nopalllfd <Naufalfadhilah689@gmail.com>
2026-05-03 21:27:07 +0700

    feat: add tugas 4

---
+++ internals/empat/geometry.go
@@ -0,0 +1,23 @@
+package empat
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Circle struct {
+	Radius int
+}
+type Rectangle struct {
+	Height int
+	Width  int
+}
+
+func (c *Circle) Area() float64 {
+	radiusQuadratic := float64(c.Radius * 2)
+	return float64(math.Pi * radiusQuadratic)
+}
+func (r *Rectangle) Area() float64 {
+	return float64(r.Height * r.Width)
+}
